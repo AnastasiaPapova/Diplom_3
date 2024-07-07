d5e08a8ac0020f792eb99a301ecc7c8295f141ee
Nastia Papova <kotishka2012@yandex.ru>
2024-07-07 17:34:50 +0300

    [Diplom_3] removed commented code

--- locators/__init__.py
+++
@@ -1,6 +0,0 @@
-# from locators.burger_locators import *
-#
-#
-# class WebUILocators(MainPageLocators, LoginUserPageLocators, ForgotPasswordlocators, ProfileLocators,
-#                     OrdersLocators):
-#     pass
